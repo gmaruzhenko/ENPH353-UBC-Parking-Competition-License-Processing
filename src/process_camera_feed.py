8c4761489504b84bd3e4f29dd1c8d7e92cbf8b83
Tyler Keeling <tylerwkeeling@gmail.com>
2019-11-25 15:54:48 -0800

    Perspective incorrect

--- src/process_camera_feed.py
+++ src/process_camera_feed.py
@@ -54,40 +54,19 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        output, (x,y,h,w) = colormask_contour(cv_image)
+        output, points = colormask_contour(cv_image)
 
-        if (x != 0 and y !=0):
-            plateimg = trim_plate(cv_image, x,y,h,w)
-            # plt.figure()
-            # plt.imshow(plateimg) 
-            # plt.show()
+        if points is not None:
+            plateimg = trim_plate(cv_image, points)
             predictplate(plateimg)
 
         image_message = self.bridge.cv2_to_imgmsg(output, encoding="bgr8") #bgr8 or 8UC1
         self.image_out.publish( image_message )
-    
-    # def loadmodel(self):
-    #     json_file = open('/home/tyler/353_ws/src/license_process/src/model.json', 'r')
-    #     loaded_model_json = json_file.read()
-    #     json_file.close()
-    #     loaded_model = model_from_json(loaded_model_json)
-    #     loaded_model.load_weights("/home/tyler/353_ws/src/license_process/src/model.h5")
-    #     graph = tf.get_default_graph()
-    #     loaded_model._make_predict_function()
 
-    #     # print (loaded_model.summary())
-
-    #     loaded_model.compile(optimizer='adam',
-    #           loss='sparse_categorical_crossentropy',
-    #           metrics=['accuracy'])
-
-    #     return loaded_model
-
-def revto(tuple, scale=1):
+def revto(tuple, scale=1): # Just to draw the openCV rectangles
     return (tuple[1] + pipe_x*scale, tuple[0] + pipe_y*scale)
 
 def predictplate(plateimg):
-    print(plateimg.shape)
     # Must not exceet 450y by 300x, pipexy currently 40 -> 80
     plate1 = (200,0)
     plate2 = (200,40)
@@ -108,8 +87,6 @@ def predictplate(plateimg):
     im4 = feature_image(plateimg,plate4,2)
     im5 = feature_image(plateimg,location,3)
 
-
-
     # Are now loading the model each time to avoid the multithreading bug
     json_file = open('/home/tyler/353_ws/src/license_process/src/model.json', 'r')
     loaded_model_json = json_file.read()
@@ -124,13 +101,13 @@ def predictplate(plateimg):
             metrics=['accuracy'])
 
     with graph.as_default():
-        p1 = loaded_model.predict(im1)
-        p2 = loaded_model.predict(im2)
-        p3 = loaded_model.predict(im3)
-        p4 = loaded_model.predict(im4)
-        p5 = loaded_model.predict(im5)
-
-        prediction = tochar(np.argmax(p1)) + tochar(np.argmax(p2)) + tochar(np.argmax(p3)) + tochar(np.argmax(p4)) + "_loc_" + str(np.argmax(p5))
+        # p1 = loaded_model.predict(im1)
+        # p2 = loaded_model.predict(im2)
+        # p3 = loaded_model.predict(im3)
+        # p4 = loaded_model.predict(im4)
+        # p5 = loaded_model.predict(im5)
+        # prediction = tochar(np.argmax(p1)) + tochar(np.argmax(p2)) + tochar(np.argmax(p3)) + tochar(np.argmax(p4)) + "_loc_" + str(np.argmax(p5))
+        prediction = str(rospy.get_rostime())
 
         if logging:
             print("Plate Found: ", prediction)
@@ -148,26 +125,23 @@ def feature_image(img, featureloc, scale=1):
     newimg = np.expand_dims(newimg, axis=4)
     return newimg
 
-def trim_plate(img, x,y,h,w):
+def trim_plate(img, points):
+    border = 20
     res_x = 300
     res_y = int(res_x*1.5)
 
-    # pts1 = np.float32([     Need this only for the non rectangle images 
-    #                 [x,y], 
-    #                 [x+w,y], 
-    #                 [x,y+h], 
-    #                 [x+h,y+h]
-    #                 ])
-    
-    # pts2 = np.float32([[0,0], [res_x,0], [0,res_y], [res_x,res_y]])
-    # pts2 += int(border/2)
-    
-    # M = cv2.getPerspectiveTransform(pts1, pts2)
-    # dst = cv2.warpPerspective(img, M, (res_x+border,res_y+border))
-    crop = img[ y:y+h, x:x+w ]
-    # crop = img[ (y-border):(y+h+border), (x-border):(x+w+border) ]
-    scale = cv2.resize(crop,(res_x,res_y))
-    im_gray = cv2.cvtColor(scale, cv2.COLOR_BGR2GRAY) # wondering if some contrast would be nice here
+    #This depends on how the plate is interpreted
+    pts1 = np.float32([np.squeeze(points[0]), np.squeeze(points[1]), 
+    np.squeeze(points[2]), np.squeeze(points[3])])
+    pts2 = np.float32([[0,0], [res_x,0], [0,res_y], [res_x,res_y]])
+    pts2 += int(border/2)
+
+    # print(pts1, pts2)
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    dst = cv2.warpPerspective(img, M, (res_x+border,res_y+border))
+
+    im_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY) # wondering if some contrast would be nice here
 
     return im_gray
 
@@ -184,6 +158,55 @@ def tochar(inty):
     else:
         return chr(55+inty) 
 
+def colormask_contour(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # HSV MASKS
+    lower_red = np.array([0,0,90]) # white plates
+    upper_red = np.array([255,60,220])
+
+    lower_black = np.array([0,0,0])
+    upper_black = np.array([255,10,20])
+    
+    colormask = cv2.inRange(hsv, lower_red, upper_red)     
+    blackmask = cv2.inRange(hsv,lower_black,upper_black)
+    bwmask = cv2.bitwise_or(colormask, blackmask)
+
+    boxmask = purplemask(img, stripes=True)
+    mask = cv2.bitwise_and(bwmask, boxmask)
+
+    kernel = np.ones((2,2),np.uint8)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # denoises the mask
+
+    _, contours, h = cv2.findContours(opening, 1, 2)
+    for cnt in contours: 
+        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+        if len(approx)==4:
+            x,y,w,h = cv2.boundingRect(cnt)
+            # h = int(h*1.37)
+            if w > 60 and h > 80 and notEdges(x,y,w,h):
+                return img, orderedpoints(approx, x,y,w,h)
+
+    return img, None
+
+def orderedpoints(approx, x,y,w,h):
+    retarr = np.zeros((4,2))
+    # I know this is inneficient but it's 4 things so chill
+    retarr[0] = mindist(approx, x,y)
+    retarr[1] = mindist(approx, x+w,y)
+    retarr[2] = mindist(approx, x,y+h)
+    retarr[3] = mindist(approx, x+w,y+h)
+
+def mindist(approx, x, y): #min of dist is min of dist^2
+    mini = 5000 # should be fine as image is 1280pix in largest dimension
+    i = 0 
+    for j in range(approx.shape[0]):
+        dist2 = abs(x - approx[j][0][0]) + abs(y - approx[i][0][1]) # Assuming contours returns XY
+        if dist2 < mini:  # if not change it upstream, no harm
+            i = j 
+            mini = dist2
+    return np.squeeze(approx[i])
+
 def notEdges(x, y, w, h, im_w=1280, im_h=720):
     if x <= 0:
         return False
@@ -195,41 +218,6 @@ def notEdges(x, y, w, h, im_w=1280, im_h=720):
         return False
     return True
 
-def colormask_contour(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # HSV MASKS
-    lower_red = np.array([0,0,90])
-    upper_red = np.array([255,50,200])
-
-    lower_black = np.array([0,0,0])
-    upper_black = np.array([255,10,20])
-    
-    colormask = cv2.inRange(hsv, lower_red, upper_red) # actually white lol
-    blackmask = cv2.inRange(hsv,lower_black,upper_black)
-    bwmask = cv2.bitwise_or(colormask, blackmask)
-
-    boxmask = purplemask(img, stripes=True)
-    mask = cv2.bitwise_and(bwmask, boxmask)
-
-    kernel = np.ones((2,2),np.uint8)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # denoises the mask
-
-    _, contours, h = cv2.findContours(opening, 1, 2)
-
-    corners = (0,0,0,0)
-    for cnt in contours: # hopefully only one rectangle is being output eek will fix later
-        #cv2.drawContours(img,[cnt],0,(0,0,255),-1)
-        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
-        if len(approx)==4:
-            x,y,w,h = cv2.boundingRect(cnt)
-            h = int(h*1.37)
-            if w > 80 and h > 100 and notEdges(x,y,w,h):
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                corners = (x,y,h,w)
-
-    return img, corners
-
 def purplemask(img, stripes=False):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_purple = np.array([120,30,30])
@@ -239,16 +227,15 @@ def purplemask(img, stripes=False):
     kernel = np.ones((2,2),np.uint8)
     opening = cv2.morphologyEx(purplemask, cv2.MORPH_OPEN, kernel)
 
-    #Generate the janky mask around the purple values s.t. we can only find plates. 
+    #Generate the stretch mask around the purple regions s.t. we can only find plates. 
     if stripes:
         overmask = np.zeros(purplemask.shape, np.dtype('uint8'))
         _, contours, h = cv2.findContours(opening, 1, 2)
 
         stretchbox = 100
-        cutpix = 40 # hopefully to get rid of the bottom part of the thing
+        cutpix = 40 # to get rid of the small bottom part
 
         for cnt in contours:
-            #cv2.drawContours(img,[cnt],0,(0,0,255),-1)
             x,y,w,h = cv2.boundingRect(cnt)
             x -= stretchbox
             w += 2*stretchbox
