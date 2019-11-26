a600de0451b7144fdaf3ea9a6e14071040fb2e76
Tyler Keeling <tylerwkeeling@gmail.com>
2019-11-26 13:43:20 -0800

    Which CornerS??!?!?

--- src/process_camera_feed.py
+++ src/process_camera_feed.py
@@ -26,17 +26,10 @@ from tensorflow.keras.models import model_from_json
 from matplotlib import pyplot as plt
 import os
 
-logging = True
+logging = True 
 
-expected_error_max = 100
-
-Kernel_size = 15
-low_threshold = 75
-high_threshold = 110
-bwThresh = 100
-
-pipe_x = 80
-pipe_y = 80
+im_w = 1280
+im_h = 720
 
 class image_converter:
 
@@ -63,29 +56,19 @@ class image_converter:
         image_message = self.bridge.cv2_to_imgmsg(output, encoding="bgr8") #bgr8 or 8UC1
         self.image_out.publish( image_message )
 
-def revto(tuple, scale=1): # Just to draw the openCV rectangles
-    return (tuple[1] + pipe_x*scale, tuple[0] + pipe_y*scale)
-
 def predictplate(plateimg):
-    # Must not exceet 450y by 300x, pipexy currently 40 -> 80
+    # Must not exceet 450y by 300x, pipexy currently 80
     plate1 = (200,0)
     plate2 = (200,40)
     plate3 = (200,140)
     plate4 = (200,220)
-    location = (180,180)
+    location = (180,140)
 
-    if logging:
-        show = cv2.rectangle(plateimg.copy(), revto(plate1), revto(plate1,2), (0,255,0), 2)
-        show = cv2.rectangle(show, revto(plate2), revto(plate2,2), (0,255,0), 2)
-        show = cv2.rectangle(show, revto(plate3), revto(plate3,2), (0,255,0), 2)
-        show = cv2.rectangle(show, revto(plate4), revto(plate4,2), (0,255,0), 2)
-        show = cv2.rectangle(show, revto(location), revto(location,2), (0,255,0), 2)
-
-    im1 = feature_image(plateimg,plate1,2) # crops
-    im2 = feature_image(plateimg,plate2,2)
-    im3 = feature_image(plateimg,plate3,2)
-    im4 = feature_image(plateimg,plate4,2)
-    im5 = feature_image(plateimg,location,3)
+    im1 = feature_image(plateimg,plate1,1) # cropping
+    im2 = feature_image(plateimg,plate2,1)
+    im3 = feature_image(plateimg,plate3,1)
+    im4 = feature_image(plateimg,plate4,1)
+    im5 = feature_image(plateimg,location,2)
 
     # Are now loading the model each time to avoid the multithreading bug
     json_file = open('/home/tyler/353_ws/src/license_process/src/model.json', 'r')
@@ -101,49 +84,37 @@ def predictplate(plateimg):
             metrics=['accuracy'])
 
     with graph.as_default():
-        # p1 = loaded_model.predict(im1)
-        # p2 = loaded_model.predict(im2)
-        # p3 = loaded_model.predict(im3)
-        # p4 = loaded_model.predict(im4)
-        # p5 = loaded_model.predict(im5)
-        # prediction = tochar(np.argmax(p1)) + tochar(np.argmax(p2)) + tochar(np.argmax(p3)) + tochar(np.argmax(p4)) + "_loc_" + str(np.argmax(p5))
-        prediction = str(rospy.get_rostime())
+        p1 = loaded_model.predict(im1)
+        p2 = loaded_model.predict(im2)
+        p3 = loaded_model.predict(im3)
+        p4 = loaded_model.predict(im4)
+        p5 = loaded_model.predict(im5)
+        prediction = tochar(np.argmax(p1)) + tochar(np.argmax(p2)) + tochar(np.argmax(p3)) + tochar(np.argmax(p4)) + "_loc_" + str(np.argmax(p5))
 
         if logging:
             print("Plate Found: ", prediction)
-            cv2.imwrite('guesses/' + prediction + '.png', show)
+            cv2.imwrite('guesses/' + prediction + '_' + str(rospy.get_rostime()) + '.png', plateimg)
 
-def feature_image(img, featureloc, scale=1):
+
+# function that returns rectangular snippet of an image based on top xy coordinate and 
+def feature_image(img, featureloc, scale=1, pipe_x=80, pipe_y=80):
+    
     Ypoint = featureloc[0]
     Xpoint = featureloc[1]
 
     Ydown   = slice(Ypoint,Ypoint+pipe_y*scale, scale)
     Xacross = slice(Xpoint,Xpoint+pipe_x*scale, scale)
 
+    pt1 = (Xpoint, Ypoint)
+    pt2 = (Xpoint + pipe_x*scale, Ypoint + pipe_x*scale)
+
+    # cv2.rectangle(img,pt1,pt2,(0,255,0),1)
+
     newimg = img[Ydown,Xacross]
     newimg = np.expand_dims(newimg, axis=0)
     newimg = np.expand_dims(newimg, axis=4)
+
     return newimg
-
-def trim_plate(img, points):
-    border = 20
-    res_x = 300
-    res_y = int(res_x*1.5)
-
-    #This depends on how the plate is interpreted
-    pts1 = np.float32([np.squeeze(points[0]), np.squeeze(points[1]), 
-    np.squeeze(points[2]), np.squeeze(points[3])])
-    pts2 = np.float32([[0,0], [res_x,0], [0,res_y], [res_x,res_y]])
-    pts2 += int(border/2)
-
-    # print(pts1, pts2)
-
-    M = cv2.getPerspectiveTransform(pts1, pts2)
-    dst = cv2.warpPerspective(img, M, (res_x+border,res_y+border))
-
-    im_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY) # wondering if some contrast would be nice here
-
-    return im_gray
 
 def toint(char):
     if char.isalpha():
@@ -157,6 +128,44 @@ def tochar(inty):
         return str(inty)
     else:
         return chr(55+inty) 
+
+
+
+
+
+
+def trim_plate(img, points):
+    border = 0
+    res_x = 300
+    res_y = int(res_x*1.5)
+    plate_start = int(res_y*0.730)
+
+    #This depends on how the plate is interpreted
+    pts0 = np.float32([np.squeeze(points[0]), np.squeeze(points[1]), 
+    np.squeeze(points[2]), np.squeeze(points[3])])
+
+        # order the points according to pts2
+    pts1 = np.float32([ mindist(pts0, 0, 0),
+                        mindist(pts0, im_w, 0),
+                        mindist(pts0, 0, im_h),
+                        mindist(pts0, im_w, im_h) ])
+    
+
+    print(pts1)
+
+    for i in range(4): # this experiment shows that points are found but placed in no order
+        cv2.circle(img, (int(pts1[i][0]),int(pts1[i][1])), 2, (255,255,255), (5*i+3))
+
+
+    pts2 = np.float32([[0,0], [res_x,0], [0,plate_start], [res_x,plate_start]])
+    pts2 += int(border/2)
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    dst = cv2.warpPerspective(img, M, (res_x+border,res_y+border))
+
+    im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # wondering if some contrast would be nice here
+    return im_gray
+
 
 def colormask_contour(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -185,27 +194,22 @@ def colormask_contour(img):
             x,y,w,h = cv2.boundingRect(cnt)
             # h = int(h*1.37)
             if w > 60 and h > 80 and notEdges(x,y,w,h):
-                return img, orderedpoints(approx, x,y,w,h)
+                return img, approx
 
     return img, None
 
-def orderedpoints(approx, x,y,w,h):
-    retarr = np.zeros((4,2))
-    # I know this is inneficient but it's 4 things so chill
-    retarr[0] = mindist(approx, x,y)
-    retarr[1] = mindist(approx, x+w,y)
-    retarr[2] = mindist(approx, x,y+h)
-    retarr[3] = mindist(approx, x+w,y+h)
-
 def mindist(approx, x, y): #min of dist is min of dist^2
     mini = 5000 # should be fine as image is 1280pix in largest dimension
-    i = 0 
+    i = 0
     for j in range(approx.shape[0]):
-        dist2 = abs(x - approx[j][0][0]) + abs(y - approx[i][0][1]) # Assuming contours returns XY
-        if dist2 < mini:  # if not change it upstream, no harm
+        dist = abs(x - approx[j][0]) + abs(y - approx[i][1]) # Assuming contours returns XY
+        if dist < mini:  
             i = j 
-            mini = dist2
-    return np.squeeze(approx[i])
+            mini = dist
+
+    print (approx[i])
+    print (mini)
+    return approx[i]
 
 def notEdges(x, y, w, h, im_w=1280, im_h=720):
     if x <= 0:
@@ -244,6 +248,14 @@ def purplemask(img, stripes=False):
 
         return overmask
     return opening
+
+
+
+
+
+
+
+
 
 def main(args):
     rospy.init_node('image_converter', anonymous=True)
