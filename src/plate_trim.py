741a91466b13d7cac7c7cc909b559b95d5cd024f
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-11-26 14:14:11 -0800

    all working but incorrectly

--- src/plate_trim.py
+++ src/plate_trim.py
@@ -14,6 +14,8 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Int16, String
 from matplotlib import pyplot as plt
 
+im_w = 1280
+im_h = 720
 
 
 def trim_plate(img, points):
@@ -48,11 +50,99 @@ def trim_plate(img, points):
     im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # wondering if some contrast would be nice here
     return im_gray
 
+def colormask_contour(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # HSV MASKS
+    lower_red = np.array([0,0,90]) # white plates
+    upper_red = np.array([255,60,220])
+
+    lower_black = np.array([0,0,0])
+    upper_black = np.array([255,10,20])
+    
+    colormask = cv2.inRange(hsv, lower_red, upper_red)     
+    blackmask = cv2.inRange(hsv,lower_black,upper_black)
+    bwmask = cv2.bitwise_or(colormask, blackmask)
+
+    boxmask = purplemask(img, stripes=True)
+    mask = cv2.bitwise_and(bwmask, boxmask)
+
+    kernel = np.ones((2,2),np.uint8)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # denoises the mask
+
+    _, contours, h = cv2.findContours(opening, 1, 2)
+    for cnt in contours: 
+        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+        if len(approx)==4:
+            x,y,w,h = cv2.boundingRect(cnt)
+            # h = int(h*1.37)
+            if w > 60 and h > 80 and notEdges(x,y,w,h):
+                return img, approx
+
+    return img, None
+
+def mindist(approx, x, y): #min of dist is min of dist^2
+    mini = 5000 # should be fine as image is 1280pix in largest dimension
+    i = 0
+    for j in range(approx.shape[0]):
+        dist = abs(x - approx[j][0]) + abs(y - approx[i][1]) # Assuming contours returns XY
+        if dist < mini:  
+            i = j 
+            mini = dist
+
+    print (approx[i])
+    print (mini)
+    return approx[i]
+
+def notEdges(x, y, w, h, im_w=1280, im_h=720):
+    if x <= 0:
+        return False
+    if y <= 0:
+        return False
+    if (x + w) >= im_w:
+        return False
+    if (y + h) >= im_h:
+        return False
+    return True
+
+def purplemask(img, stripes=False):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_purple = np.array([120,30,30])
+    upper_purple = np.array([130,255,255])
+    purplemask = cv2.inRange(hsv, lower_purple, upper_purple)
+
+    kernel = np.ones((2,2),np.uint8)
+    opening = cv2.morphologyEx(purplemask, cv2.MORPH_OPEN, kernel)
+
+    #Generate the stretch mask around the purple regions s.t. we can only find plates. 
+    if stripes:
+        overmask = np.zeros(purplemask.shape, np.dtype('uint8'))
+        _, contours, h = cv2.findContours(opening, 1, 2)
+
+        stretchbox = 100
+        cutpix = 40 # to get rid of the small bottom part
+
+        for cnt in contours:
+            x,y,w,h = cv2.boundingRect(cnt)
+            x -= stretchbox
+            w += 2*stretchbox
+            h -= cutpix
+            cv2.rectangle(overmask,(x,y),(x+w,y+h),(255), cv2.FILLED)
+
+        return overmask
+    return opening
+
 def main(args):
     image_raw = cv2.imread('../../../dataset/1574312167.41@DT24.png')
     cv2.imshow('test',image_raw)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+   
 
+    output, points = colormask_contour(image_raw)
+    trimmed_image = trim_plate(image_raw,points)
+    cv2.imshow('timmed',trimmed_image)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
 if __name__ == '__main__':
     main(sys.argv)
